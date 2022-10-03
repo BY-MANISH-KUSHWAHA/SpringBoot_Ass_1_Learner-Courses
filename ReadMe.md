74b7dfb22ef3e58637720bbead66f27c5faa9d9f
BY-MANISH-KUSHWAHA <manish.kushwaha@blueyonder.com>
2022-10-03 08:31:50 +0530

    Done

---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+# Learner and Course
+
+# Get All Learners - JPA
+![GetAllLearners](assets/1.PNG)
+
+
+# Get All Courses - JPA
+![GetAllLearners](assets/7.PNG)
+
+
+# Get Learner By ID - JPA
+![GetLearnerById](assets/2.PNG)
+
+
+# Get Course By Id - JPA
+![GetCourseById](assets/14.PNG)
+
+
+# Is Learner Exist By ID - JPA
+![GetLearnerById](assets/2.PNG)
+
+
+# Is Course Exist By Id - JPA
+![GetCourseById](assets/14.PNG)
+
+
+
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(1, null ,'Manish','Kushwaha','mk9313@gmail.com','password');
-INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(2, null ,'Megha','Kanchi','mk7234@gmail.com','password');
-INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(3, null ,'Shubham','Manjhi','sm1567@gmail.com','password');
+INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(2, null ,'Rathor','Kanchi','mk7234@gmail.com','password');
+INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(3, null ,'Manish','Manjhi','sm1567@gmail.com','password');
 INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(4, null ,'Swarti','Rathor','sr9876@gmail.com','password');
-INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(5, null ,'Pratham','Kushwah','pk2635@gmail.com','password');
+INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(5, null ,'Arya','Kushwaha','pk2635@gmail.com','password');
 INSERT INTO Learner(id, course_id, first_Name, last_Name, email, password) VALUES(6, null ,'Mehak','Sharma','ms0685@gmail.com','password');
 INSERT INTO Course(id, name, description) VALUES(1, 'Linear Algebra' ,'Matrix Multiplication');
 INSERT INTO Course(id, name, description) VALUES(2, 'Convex Optimization' ,'Solve 4 unknown variable');
